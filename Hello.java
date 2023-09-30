66309562bf40a2d58dd1ae15e07313cd88001acc
Xin <3161409269@qq.com>
2023-09-30 18:02:27 +0800

    second commit Helllo.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("你好呀，Hello,World");
+    System.out.println("git commit second");
   }
 }
