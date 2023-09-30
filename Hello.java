c25723e9f976dbb749dba65b141745fabfbf9359
Xin <3161409269@qq.com>
2023-09-30 18:19:15 +0800

    third commit Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("你好呀，Hello,World");
-    System.out.println("git commit second");
+    System.out.println("git commit third");
     System.out.println("热修复的分支的建立");
   }
 }
