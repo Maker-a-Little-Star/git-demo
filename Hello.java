22d6deca486969a6ad9d338c751997536c03cfdd
Xin <3161409269@qq.com>
2023-09-30 18:13:45 +0800

    hot-fix first commit Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("你好呀，Hello,World");
     System.out.println("git commit second");
+    System.out.println("热修复的分支的建立");
   }
 }
